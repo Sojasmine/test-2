7667cd2f341d3b614dede7690633e261e6ea7f3e
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-08-28 13:15:41 +0000

    Reinstalled and installed the products folder. Problem installing the fixture files. Added new images.

--- alana/settings.py
+++ alana/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'alana.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlites',
     }
 }
 
@@ -150,8 +150,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
